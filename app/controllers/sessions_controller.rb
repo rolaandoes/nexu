5df01a8d629794faeb37c3e3a6ab5336e0f8334e
GregOSF <gregory.onza@gmail.com>
2015-08-19 09:59:04 -0700

    Added create new auth and commented out oauth for testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,46 @@
 class SessionsController < ApplicationController
+  
+  def new 
+    if current_user
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
+
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
+      # user = User.from_omniauth(env["omniauth.auth"])
+      # session[:user_id] = user.id
+      # if current_user
+      
+       user=User.find_by_name(user_params[:name])
+       session[:user_id] = user.id
+       redirect_to profile_path
+      # end
+  end
+
+
+
+
+        # if user && user.authenticate(user_params[:password])
+        #  session[:user_id]=user.id
+        #  redirect_to profile_path
+        # else
+        #   flash[:error] = "Failed To Authenticate. Please try again."
+        #   redirect_to login_path
+        # end
+    # end
     # redirect_to root_url
     # if user has phone #
     # if user.phone
     # redirect_to '/profile'
     # else
-    if user.phone_number
-      redirect_to '/users/:user_id'
-    else
-      redirect_to '/users/new'
-    end
-  end
+    # if user.phone_number
+    #   redirect_to '/users/:user_id'
+    # else
+    #   redirect_to '/users/new'
+    # end
+  
 
   def new
   end
@@ -21,4 +49,9 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
