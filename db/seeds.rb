7309023501d023b74e229f22841efce59213ccf0
GregOSF <gregory.onza@gmail.com>
2015-08-19 10:07:50 -0700

    Added seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Interest.create(topic: "Technology")
-Interest.create(topic: "Outdoors")
-Interest.create(topic: "Games")
-Interest.create(topic: "Parties")
-Interest.create(topic: "Politics")
+# Interest.create(topic: "Technology")
+# Interest.create(topic: "Outdoors")
+# Interest.create(topic: "Games")
+# Interest.create(topic: "Parties")
+# Interest.create(topic: "Politics")
+
+
+
+# 25.times do
+#     User.create(
+#       name: FFaker::Name.name,
+#       phone_number: FFaker::PhoneNumber.phone_number,
+#       description: FFaker::HipsterIpsum.sentence(5),
+#       gender: FFaker::Gender.random,
+#       interest_id: rand(1..5)
+#     )
+# end
+
