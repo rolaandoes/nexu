58baace8b8e652c725cbe3cdda188935d489d58b
GregOSF <gregory.onza@gmail.com>
2015-08-19 10:07:31 -0700

    Added interest FK to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817230150) do
+ActiveRecord::Schema.define(version: 20150819044603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150817230150) do
     t.integer  "phone_number"
     t.text     "description"
     t.string   "gender"
+    t.integer  "interest_id"
   end
 
 end
