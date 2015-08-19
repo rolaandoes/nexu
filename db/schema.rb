1d1d1288a4671c643d8ab46267f77a057efe1fe0
GregOSF <gregory.onza@gmail.com>
2015-08-19 14:00:25 -0700

    Added columns for lat and lon

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150819044603) do
-
 ActiveRecord::Schema.define(version: 20150819093014) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "interests", force: :cascade do |t|
     t.string   "topic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.float    "latitude"
+    t.float    "longtitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -37,12 +40,7 @@ ActiveRecord::Schema.define(version: 20150819093014) do
     t.integer  "phone_number"
     t.text     "description"
     t.string   "gender"
-
     t.integer  "interest_id"
-
-    t.string   "latitude"
-    t.string   "longitude"
-
   end
 
 end
