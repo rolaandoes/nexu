69e06f8e6319008f26a5d962575a62c68fdb6ab0
Jon Harlan <jonharlan@JONATHANs-MacBook-Pro-2.local>
2015-08-19 19:05:56 -0700

    Starting Connections Algorithm

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
+  get '/connections', to: "matches#connection"
+
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
@@ -38,33 +40,33 @@ Rails.application.routes.draw do
 
 end
 
-
-#        Prefix Verb     URI Pattern                        Controller#Action
-#          root GET      /                                  pages#index
-#               GET|POST /auth/:provider/callback(.:format) sessions#create
-#  auth_failure GET|POST /auth/failure(.:format)            redirect(301, /)
-#       signout GET|POST /signout(.:format)                 sessions#destroy
-#         users GET      /users(.:format)                   users#index
-#               POST     /users(.:format)                   users#create
-#      new_user GET      /users/new(.:format)               users#new
-#     edit_user GET      /users/:id/edit(.:format)          users#edit
-#          user GET      /users/:id(.:format)               users#show
-#               PATCH    /users/:id(.:format)               users#update
-#               PUT      /users/:id(.:format)               users#update
-#               DELETE   /users/:id(.:format)               users#destroy
-#       profile GET      /profile(.:format)                 users#show
-#        signup GET      /signup(.:format)                  sessions#new
-#    my_matches GET      /my-matches(.:format)              matches#edit
-#       find_me PATCH    /find-me(.:format)                 locations#show
-#     locations GET      /locations(.:format)               locations#index
-#               POST     /locations(.:format)               locations#create
-#  new_location GET      /locations/new(.:format)           locations#new
-# edit_location GET      /locations/:id/edit(.:format)      locations#edit
-#      location GET      /locations/:id(.:format)           locations#show
-#               PATCH    /locations/:id(.:format)           locations#update
-#               PUT      /locations/:id(.:format)           locations#update
-#               DELETE   /locations/:id(.:format)           locations#destroy
-
-
+#       Prefix Verb     URI Pattern                        Controller#Action
+#         root GET      /                                  pages#index
+#              GET|POST /auth/:provider/callback(.:format) sessions#create
+# auth_failure GET|POST /auth/failure(.:format)            redirect(301, /)
+#      signout GET|POST /signout(.:format)                 sessions#destroy
+#      profile GET      /profile(.:format)                 users#show
+#   users_edit GET      /users/edit(.:format)              users#edit
+#              POST     /users/edit(.:format)              users#update
+#         user PUT      /users/edit(.:format)              users#update
+#        users GET      /users(.:format)                   users#index
+#              POST     /users(.:format)                   users#create
+#     new_user GET      /users/new(.:format)               users#new
+#    edit_user GET      /users/:id/edit(.:format)          users#edit
+#              GET      /users/:id(.:format)               users#show
+#              PATCH    /users/:id(.:format)               users#update
+#              PUT      /users/:id(.:format)               users#update
+#              DELETE   /users/:id(.:format)               users#destroy
+#      matches GET      /matches(.:format)                 matches#index
+#              POST     /matches(.:format)                 matches#create
+#    new_match GET      /matches/new(.:format)             matches#new
+#   edit_match GET      /matches/:id/edit(.:format)        matches#edit
+#        match GET      /matches/:id(.:format)             matches#show
+#              PATCH    /matches/:id(.:format)             matches#update
+#              PUT      /matches/:id(.:format)             matches#update
+#              DELETE   /matches/:id(.:format)             matches#destroy
+#       signup GET      /signup(.:format)                  sessions#new
+#   my_matches GET      /my-matches(.:format)              matches#edit
+#      find_me GET      /find-me(.:format)                 users#edit
 
 
