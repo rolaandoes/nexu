ba6fc4ad08f47ea749f81f06d91c403269d5472b
Jon Harlan <jonharlan@JONATHANs-MacBook-Pro-2.local>
2015-08-20 12:53:56 -0700

    about to change datatype for phone#

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,24 +16,12 @@ class UsersController < ApplicationController
     # p @user
   end
 
-
-
   def update
-    # byebug
-    # @user.update_attributes(user_params)
-    # user_id = current_user.id
-    # @user = User.find(user_id)
     @user = User.find(session[:user_id])
+    p @user
     @user.update_attributes(user_params)
-    # redirect_to profile_path
     render :edit
   end
-
-
-
-
-
-
   # - - - - - - - - - - - - - - - - - - 
 
   def show
@@ -46,56 +34,9 @@ class UsersController < ApplicationController
 #==========================
   private
     def user_params
-      params.require(:user).permit(:latitude, :longitude, :interest_id, :interest, :image)
+      params.require(:user).permit(:latitude, :longitude, :interest_id, :interest, :image, :phone_number, :description)
     end
 
 
 
 end
-
-#====================================
-  # def update
-  #   post_id = params[:id]
-  #   post = Post.friendly.find(post_id)
-  #   updated_attributes = post_params
-
-  #   if post.update_attributes(updated_attributes)
-  #     #updated attr...
-  #       redirect_to post_path
-  #   end
-  # end
-
-  # def update
-  #   # @user = User.find(session[:user_id])
-  #   # skip_before_action :verify_authenticity_token
-  #   # user_id = current_user.id
-  #   # @user = User.find(user_id)
-  #   # @user.update_attributes(user_params)
-
-  #   # puts @user.latitude
-  #   @user.update_attributes(user_params)
-  # end
-
-  # def location
-  #   @lat
-  #   @lon
-
-  # end
-
-  # puts update.updated_attributes
-
-  # Users Table Migration => add colomns : latitude, longitude
-
-  # Need to get Scripts.js talking with Users_controller (Here)
-# -----------
-  # Button: Send My Location 
-    #Send latitude and longitude to User Table in DB
-
-  # Button: Am I Close to Anyone?
-    #Runs Distance Function
-      #in which File?
-
-
-
-
-
