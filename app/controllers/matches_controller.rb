c52cf4194a7bb19b8c3c41b3ae1b294323380f60
Michelle Maltbia <maltbia.michelle@gmail.com>
2015-08-20 15:51:58 -0700

    adds styling to almost complete app

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,7 +2,7 @@ class MatchesController < ApplicationController
 
 	def index
 		@users = []
-		all_users = User.all
+		all_users = User.all - [current_user]
 		current_user = User.find(session[:user_id])
 		all_users.each do |user|
 			if user.interest_id == current_user.interest_id
@@ -71,7 +71,7 @@ end
 
 
   def show
-  	# @users = User.find(current_user.interest_id)
+  	@user = User.find(current_user[:interest_id])
   end
 
   def edit
